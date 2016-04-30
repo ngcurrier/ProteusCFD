48b442901acc1f10c865ab738348e14bd633a045
Nick Currier <nicholas.currier@gmail.com>
2016-04-29 20:10:02 -0700

    Moved bcTab widget to its own file for clarity

---
+++ GUI/BCTab.py
@@ -0,0 +1,98 @@
+from guiData import *
+from PyQt4 import QtCore, QtGui
+import functools
+
+class BCTab(QtGui.QWidget):
+    """This class represents the bc tab"""
+
+    # pass in the gui data object and a function to change visibility of vtk parts
+    def __init__(self, data, visibilityChangeFunc):
+        self.data = data
+        super(BCTab, self).__init__()
+        addBCButton = QtGui.QPushButton('Add Boundary Condition', self)
+        addBCButton.clicked.connect(functools.partial(self.BoundaryConditionPopup, self.data))
+        addBCButton.setToolTip('Add a new boundary condition')
+        addBCButton.resize(addBCButton.sizeHint())
+        addBCButton.move(10, 10)
+        # This loop assumes that the vtk parts are created in the same order as the factags
+        irow = 0
+        for tag in self.data.elemGroups:
+            if int(tag) >= 0:
+                checkViz = QtGui.QCheckBox('factag: ' + str(tag), self)
+                checkViz.setChecked(True)
+            else:
+                checkViz = QtGui.QCheckBox('voltag: ' + str(tag), self)
+                checkViz.setChecked(False) # hide volumes by default
+            checkViz.stateChanged.connect(functools.partial(visibilityChangeFunc, int(irow), int(tag)))
+            checkViz.move(20, 40+25*irow)
+            irow = irow + 1
+        self.bcwindow = None
+
+    def BoundaryConditionPopup(self, data):
+        print "Opening a new BC popup window..."
+        self.bcwindow = BCPopup(data)
+        self.bcwindow.setGeometry(QtCore.QRect(100, 100, 600, 300))
+        self.bcwindow.show()
+
+class BCPopup(QtGui.QWidget):
+    """This class represents the bc popup for BC editing"""
+    
+    def __init__(self, data):
+        QtGui.QWidget.__init__(self)
+        self.numBCsSet = len(data.elemGroups)
+        self.bcIdSelected = 0
+        self.bcTypeSelected = 'NULL'
+
+        # setup layout
+        self.vl = QtGui.QGridLayout()
+        self.vl.setSpacing(10)
+        self.setLayout(self.vl)
+
+        # List of factags (i.e. BCs to set)
+        self.bcList = QtGui.QComboBox()
+        for tag in data.elemGroups:
+            self.bcList.addItem('factag: ' + str(tag))
+        self.bcList.activated[str].connect(self.bcListActivate)
+        self.vl.addWidget(self.bcList,0,0)
+        
+        # List of bc types available
+        self.typeList = QtGui.QComboBox()
+        for type in data.bcTypes:
+            self.typeList.addItem(type)
+        self.typeList.activated[str].connect(self.bcTypeActivate)
+        self.vl.addWidget(self.typeList,1,0)
+
+        # List of current BCs set
+        bcCols = 4
+        bcRows = self.numBCsSet
+        self.bcTable = QtGui.QTableWidget()
+        self.bcTable.setRowCount(self.numBCsSet)
+        self.bcTable.setColumnCount(bcCols)
+        self.bcTable.resize(self.bcTable.sizeHint())
+        irow = 0
+        for tag in data.elemGroups:
+            self.bcTable.setItem(irow,1, QtGui.QTableWidgetItem(str(tag)))
+            irow = irow + 1
+            
+        self.bcTable.setHorizontalHeaderLabels(("BoundaryID; Factag; head3; Nickname").split(";"))
+        self.bcTable.show()
+        self.vl.addWidget(self.bcTable,2,1)
+
+
+        self.addBCButton = QtGui.QPushButton('Apply')
+        #addBCButton.clicked.connect()
+        self.addBCButton.setToolTip('Apply Boundary Condition')
+        self.addBCButton.resize(self.addBCButton.sizeHint())
+        self.addBCButton.setStyleSheet("background-color: #5BC85B")
+        self.vl.addWidget(self.addBCButton,3,1)
+
+    def bcListActivate(self, text):
+        print 'Combo selection changed to ' + text
+        self.bcIdSelected = text
+
+    def bcTypeActivate(self, text):
+        print 'Combo selection changed to ' + text
+        self.bcTypeSelected = text
+
+    def treeChanged(self, item, column):
+        print 'tree changed'
--- GUI/gui_main.py
+++ GUI/gui_main.py
@@ -4,6 +4,7 @@ import sys
 import pickle # we use pickle to save run information, etc.
 import vtk
 from guiData import *
+from BCTab import BCTab
 from PyQt4 import QtCore, QtGui
 from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from defines import *
@@ -47,25 +48,8 @@ class MainWindow(QtGui.QMainWindow):
         self.tabs.addTab(SolutionControlTab, "Solution Control")
 
         #setup boundary condition tab
-        BCTab = QtGui.QWidget()
-        self.tabs.addTab(BCTab, "Boundary Conditions")
-        addBCButton = QtGui.QPushButton('Add Boundary Condition', BCTab)
-        addBCButton.clicked.connect(self.BoundaryConditionPopup)
-        addBCButton.setToolTip('Add a new boundary condition')
-        addBCButton.resize(addBCButton.sizeHint())
-        addBCButton.move(10, 10)
-        irow = 0
-        for tag in self.data.elemGroups:
-            if int(tag) >= 0:
-                checkViz = QtGui.QCheckBox('factag: ' + str(tag), BCTab)
-                checkViz.setChecked(True)
-            else:
-                checkViz = QtGui.QCheckBox('voltag: ' + str(tag), BCTab)
-                checkViz.setChecked(False) # hide volumes by default
-            checkViz.stateChanged.connect(functools.partial(self.visibilityChanged, int(irow), int(tag)))
-            checkViz.move(20, 40+25*irow)
-            irow = irow + 1
-        self.bcwindow = None
+        BCTab1 = BCTab(self.data, self.visibilityChanged)
+        self.tabs.addTab(BCTab1, "Boundary Conditions")
         
         #draw the tabs
         self.tabs.show()
@@ -113,8 +97,10 @@ class MainWindow(QtGui.QMainWindow):
             mapper.SetInput(vtkgrid)
             self.vtkActorList.append(vtk.vtkActor())
             self.vtkActorList[-1].SetMapper(mapper)
+            # set visibility of volumes to false on startup
             if int(self.data.elemGroups[itag]) < 0:
                 self.vtkActorList[-1].VisibilityOff()
+            itag = itag + 1
             
         #visualize the grid
         for actor in self.vtkActorList:
@@ -177,72 +163,7 @@ class MainWindow(QtGui.QMainWindow):
         self.state = pickle.load(open(filename, "rb"))
         print self.state
 
-    def BoundaryConditionPopup(self):
-        print "Opening a new BC popup window..."
-        self.bcwindow = BCPopup(self.data)
-        self.bcwindow.setGeometry(QtCore.QRect(100, 100, 600, 300))
-        self.bcwindow.show()
 
-class BCPopup(QtGui.QWidget):
-    def __init__(self, data):
-        QtGui.QWidget.__init__(self)
-        self.numBCsSet = len(data.elemGroups)
-        self.bcIdSelected = 0
-        self.bcTypeSelected = 'NULL'
-
-        # setup layout
-        self.vl = QtGui.QGridLayout()
-        self.vl.setSpacing(10)
-        self.setLayout(self.vl)
-
-        # List of factags (i.e. BCs to set)
-        self.bcList = QtGui.QComboBox()
-        for tag in data.elemGroups:
-            self.bcList.addItem('factag: ' + str(tag))
-        self.bcList.activated[str].connect(self.bcListActivate)
-        self.vl.addWidget(self.bcList,0,0)
-        
-        # List of bc types available
-        self.typeList = QtGui.QComboBox()
-        for type in data.bcTypes:
-            self.typeList.addItem(type)
-        self.typeList.activated[str].connect(self.bcTypeActivate)
-        self.vl.addWidget(self.typeList,1,0)
-
-        # List of current BCs set
-        bcCols = 4
-        bcRows = self.numBCsSet
-        self.bcTable = QtGui.QTableWidget()
-        self.bcTable.setRowCount(self.numBCsSet)
-        self.bcTable.setColumnCount(bcCols)
-        self.bcTable.resize(self.bcTable.sizeHint())
-        irow = 0
-        for tag in data.elemGroups:
-            self.bcTable.setItem(irow,1, QtGui.QTableWidgetItem(str(tag)))
-            irow = irow + 1
-            
-        self.bcTable.setHorizontalHeaderLabels(("BoundaryID; Factag; head3; Nickname").split(";"))
-        self.bcTable.show()
-        self.vl.addWidget(self.bcTable,2,1)
-
-
-        self.addBCButton = QtGui.QPushButton('Apply')
-        #addBCButton.clicked.connect()
-        self.addBCButton.setToolTip('Apply Boundary Condition')
-        self.addBCButton.resize(self.addBCButton.sizeHint())
-        self.addBCButton.setStyleSheet("background-color: #5BC85B")
-        self.vl.addWidget(self.addBCButton,3,1)
-
-    def bcListActivate(self, text):
-        print 'Combo selection changed to ' + text
-        self.bcIdSelected = text
-
-    def bcTypeActivate(self, text):
-        print 'Combo selection changed to ' + text
-        self.bcTypeSelected = text
-
-    def treeChanged(self, item, column):
-        print 'tree changed'
         
         
 if __name__ == "__main__":
