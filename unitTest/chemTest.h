8a3f3fde91c4f1d251b9a650c15440c7fbaf231a
Nick Currier <nicholas.currier@gmail.com>
2018-02-23 17:29:01 -0800

    Fixes to chem test database location in tests

--- unitTest/chemTest.h
+++ unitTest/chemTest.h
@@ -1,26 +1,50 @@
 #include <gtest/gtest.h>
 #include <chem.h>
 #include <species.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <string>
 
-class ChemTestRead : public testing::Test
+class ChemSetup : public ::testing::Test
+{
+ protected:
+  ChemSetup(){};
+  ~ChemSetup(){};
+  std::string homePath;
+
+  virtual void SetUp()
+  {
+    const char *homedir;
+    if ((homedir = getenv("HOME")) == NULL) {
+      homedir = getpwuid(getuid())->pw_dir;
+    }
+    homePath = std::string(homedir);
+  };
+  void TearDown(){};
+};
+
+class ChemTestRead : public ChemSetup
 {
  protected:
   ChemTestRead(){};
   ~ChemTestRead(){};
 
-  void SetUp(){};
+  void SetUp()
+  {
+    ChemSetup::SetUp();
+  };
   void TearDown(){};
-
 };
 
 TEST_F(ChemTestRead, testChemRxnFileRead)
 {
   Int isViscous = true;
-  std::string chemdb = "/usr/local/database/chemdb.hdf5";
+  std::string chemdb = homePath + "/.proteusCFD/database/chemdb.hdf5";
   ChemModel<double> chem("../unitTest/chemResources/11speciesAir", isViscous, chemdb);
   EXPECT_EQ(11, chem.nspecies);
   EXPECT_EQ(20, chem.nreactions);
-  EXPECT_EQ(1, chem.nespecies);
+  EXPECT_EQ(0, chem.nespecies);
 
   double rhoi[11];
 
@@ -28,10 +52,23 @@ TEST_F(ChemTestRead, testChemRxnFileRead)
   
 }
 
-TEST(testSpeciesN2, testProps)
+class testSpeciesN2 : public ChemSetup
+{
+ protected:
+  testSpeciesN2(){};
+  ~testSpeciesN2(){};
+  void SetUp()
+  {
+    ChemSetup::SetUp();
+  };  
+  void TearDown(){};
+};
+
+TEST_F(testSpeciesN2, testProps)
 {
   Species<double> n2;
-  n2.Init("N2", true, "/usr/local/database/chemdb.hdf5");
+  std::string chemdb = homePath + "/.proteusCFD/database/chemdb.hdf5";
+  n2.Init("N2", true, chemdb);
  
   //1.039 kj/kg.K
   EXPECT_NEAR(1039.6, n2.GetCp(300), 1.0e-1);
@@ -41,10 +78,23 @@ TEST(testSpeciesN2, testProps)
   EXPECT_EQ(0.0, n2.charge);
 }
 
-TEST(testIdealGasEOS, testCalls)
+class testIdealGasEOS : public ChemSetup
+{
+ protected:
+  testIdealGasEOS(){};
+  ~testIdealGasEOS(){};
+  void SetUp()
+  {
+    ChemSetup::SetUp();
+  };  
+  void TearDown(){};
+};
+
+TEST_F(testIdealGasEOS, testCalls)
 {
   Species<double> n2;
-  n2.Init("N2", true, "/usr/local/database/chemdb.hdf5");
+  std::string chemdb = homePath + "/.proteusCFD/database/chemdb.hdf5";
+  n2.Init("N2", true, chemdb);
 
   IdealGasEOS<double> eos;
 
