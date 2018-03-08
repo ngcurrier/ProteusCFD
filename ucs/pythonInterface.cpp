2458c1113dbf32443daf023a4780373f4b53cc27
Nick Currier <nicholas.currier@gmail.com>
2018-03-08 09:39:32 -0800

    Updates to class'ify'the python interface to make it simpler to use

--- ucs/pythonInterface.cpp
+++ ucs/pythonInterface.cpp
@@ -2,70 +2,125 @@
 
 #ifdef _HAS_PYTHON
 
-int callPython(int argc, char* argv[]){
-
-  PyObject *pName, *pModule, *pDict, *pFunc;
-  PyObject *pArgs, *pValue;
-  int i;
-
-  if (argc < 3) {
-    fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
-    return 1;
-  }
-
+PythonWrapper::PythonWrapper(std::string fileRoot, std::string functionName)
+{
   Py_Initialize();
-  pName = PyString_FromString(argv[1]);
-  /* Error checking of pName left out */
 
+  pName = PyString_FromString(fileRoot.c_str());
   pModule = PyImport_Import(pName);
-  Py_DECREF(pName);
 
-  if (pModule != NULL) {
-    pFunc = PyObject_GetAttrString(pModule, argv[2]);
-    /* pFunc is a new reference */
+  if(pModule != NULL){
+    pFunc = PyObject_GetAttrString(pModule, functionName.c_str());
 
-    if (pFunc && PyCallable_Check(pFunc)) {
-      pArgs = PyTuple_New(argc - 3);
-      for (i = 0; i < argc - 3; ++i) {
-	pValue = PyInt_FromLong(atoi(argv[i + 3]));
-	if (!pValue) {
-	  Py_DECREF(pArgs);
-	  Py_DECREF(pModule);
-	  fprintf(stderr, "Cannot convert argument\n");
-	  return 1;
-	}
-	/* pValue reference stolen here: */
-	PyTuple_SetItem(pArgs, i, pValue);
-      }
-      pValue = PyObject_CallObject(pFunc, pArgs);
-      Py_DECREF(pArgs);
-      if (pValue != NULL) {
-	printf("Result of call: %ld\n", PyInt_AsLong(pValue));
-	Py_DECREF(pValue);
-      }
-      else {
-	Py_DECREF(pFunc);
-	Py_DECREF(pModule);
-	PyErr_Print();
-	fprintf(stderr,"Call failed\n");
-	return 1;
-      }
+    if(pFunc && PyCallable_Check(pFunc)){
+      //don't do anything
     }
-    else {
-      if (PyErr_Occurred())
+    else{
+      if(PyErr_Occurred()){
 	PyErr_Print();
-      fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+      }
+      std::cerr << "Cannot load function in PythonWrapper " << functionName << " from file " << fileRoot << std::endl;
+      //TODO: throwerror
     }
-    Py_XDECREF(pFunc);
-    Py_DECREF(pModule);
   }
-  else {
+  else{
     PyErr_Print();
-    fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-    return 1;
+    std::cerr << "PythonWrapper failed to load " << fileRoot << std::endl;
   }
+  
+  import_array(); //this is so we can use Numpy arrays
+}
+
+PythonWrapper::~PythonWrapper()
+{
+  //call xdecref in case any of these failed with a NULL return
+  // DECREF() requires a valid pointer
+  Py_XDECREF(pFunc);
+  Py_XDECREF(pModule);
+  Py_XDECREF(pName);
   Py_Finalize();
-  return 0;
+}
+
+//The python function is expected to accept a numpy array
+//The argument should (*) splat expand the pyobject args
+std::vector<double> PythonWrapper::CallDoubleVectorFunction(std::vector<double>& input)
+{
+  npy_intp inputsize = input.size();
+  PyObject* numpyarray = (PyObject*)PyArray_SimpleNewFromData(1, &inputsize, PyArray_DOUBLE, (double*)input.data());
+  PyObject* pArgs = PyTuple_New(1);
+  PyTuple_SetItem(pArgs, 0, numpyarray);
+   
+  PyObject* pValue = PyObject_CallObject(pFunc, pArgs);
+
+  PyArrayObject *np_ret = reinterpret_cast<PyArrayObject*>(pValue);
+  int size = PyArray_DIMS(pValue)[0];
+  std::cout << "Received array with dimensions " << size << std::endl;
+
+  // Convert back to C++ array and print.
+  double* c_out = reinterpret_cast<double*>(PyArray_DATA(pValue));
+  std::vector<double> returnVec(size);
+  //print and load up vector
+  std::cout << "Printing output array" << std::endl;
+  for (int i = 0; i < size; i++){
+    std::cout << c_out[i] << ' ';
+    returnVec[i] = c_out[i];
+  }
+  std::cout << std::endl;
+
+  //free references
+  Py_DECREF(pValue);
+  Py_DECREF(pArgs);
+  Py_DECREF(numpyarray);
+    
+  return returnVec;
+}
+
+double PythonWrapper::CallTwoIntFunction(int a, int b)
+{
+  PyObject* pArgs;
+  PyObject* pValue;
+  double returnVal;
+
+  pArgs = PyTuple_New(2);
+  pValue = PyInt_FromLong(a);
+  if(!pValue){
+    Py_DECREF(pArgs);
+    std::cerr << "Cannot convert argument in PythonWrapper" << std::endl;
+    //TODO: call throwerror()
+    return -999;
+  }
+  else{
+    PyTuple_SetItem(pArgs, 0, pValue);
+  }
+
+  pValue = PyInt_FromLong(b);
+  if(!pValue){
+    Py_DECREF(pArgs);
+    std::cerr << "Cannot convert argument in PythonWrapper" << std::endl;
+    //TODO: call throwerror()
+    return -999;
+  }
+  else{
+    PyTuple_SetItem(pArgs, 1, pValue);
+  }
+
+  //Recall that if you need to unpack pArgs on the python side to use the (*) splat operator
+  pValue = PyObject_CallObject(pFunc, pArgs);
+
+  if(pValue == NULL){
+    PyErr_Print();
+    std::cerr << "PythonWrapper call did not return anything" << std::endl;
+    return -999;
+  }
+
+  //we expect back a float
+  //PyInt_AsLong(pValue);
+  PyFloat_AsDouble(pValue);
+
+  Py_DECREF(pValue);
+  Py_DECREF(pArgs);
+  
+  return returnVal;
 }
 
 #endif
--- ucs/pythonInterface.h
+++ ucs/pythonInterface.h
@@ -3,9 +3,32 @@
 
 #ifdef _HAS_PYTHON
 #include <Python.h>
-#endif
+#include <numpy/arrayobject.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
+class PythonWrapper
+{
+ public:
+  PythonWrapper(std::string fileRoot, std::string functionName);
+  ~PythonWrapper();
+
+  //These are demo stubouts to build from
+  std::vector<double> CallDoubleVectorFunction(std::vector<double>& input);
+  double CallTwoIntFunction(int a, int b);
+  
+ protected:
+
+ private:
+  PythonWrapper();  //hide default implementation
+  PyObject* pName;   //This is the python file name "i.e. the *.py"
+  PyObject* pModule; //This is the python module loaded of that file
+  PyObject* pFunc;   //This is the active python function handle for calling out
+};
 
 
 
+#endif // end HAS_PYTHON
 
-#endif
+#endif // end HEADER DEF
