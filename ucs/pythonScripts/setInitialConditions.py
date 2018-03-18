15261b1facfe3815d43d2925da179905406c0a36
Nick Currier <nicholas.currier@gmail.com>
2018-03-18 09:55:13 -0700

    Update form of setInitialConditions.py

--- ucs/pythonScripts/setInitialConditions.py
+++ ucs/pythonScripts/setInitialConditions.py
@@ -1,17 +1,24 @@
-def setInitialConditions(Qinf, Neqn, Qset, CoordsXYZ):
+def setInitialConditions(Qinf, neqn, naux, Qset, CoordsXYZ):
     import numpy as np
+
+    print Qinf[0]
     
     #Qinf is the raw value of farfield variables (non-dimensional) - DO NOT MODIFY
-    qinfSize = np.shape(Qinf)[1]
+    print neqn+ naux
+    qinfSize = neqn + naux
+    print 'Vector size', qinfSize
     
     #Coords array(non-dimensional) [x,y,z]
-    x = Coords[0]
-    y = Coords[1]
-    z = Coords[2]
+    x = CoordsXYZ[0]
+    y = CoordsXYZ[1]
+    z = CoordsXYZ[2]
+    print x,y,z
 
     #Note that computeAuxiliaryVariables() is called after the set to ensure consistency
     #You do not have to set those locations to anything meaningfu
-    naux = qinfSize - Neqn
+    naux = qinfSize - neqn
 
     #Qset should be set to desired values (non-dimensional)
     Qset[0] = 10.0
+
+    return 1
